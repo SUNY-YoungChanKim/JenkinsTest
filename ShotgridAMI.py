e10b7f59415bd7511d97596fd11fc80c37a99f55
SUNY-YoungChanKim <62223904+SUNY-YoungChanKim@users.noreply.github.com>
2022-06-14 11:42:36 +0900

    Update ShotgridAMI.py

--- ShotgridAMI.py
+++ ShotgridAMI.py
@@ -20,9 +20,9 @@ sg= shotgun_api3.Shotgun(
 def delSequence():
     try:
         dataDictForm= dict(request.form.to_dict())
-        selectedIds=dataDictForm['selected_ids'].split(',')                                                     #생성된 시퀸스들의 아이디를 리스트 형태로 가져옴
+        selectedIds=dataDictForm['selected_ids'].split(',')                                                     
 
-        req = {'sequence':[],'shots':[]}                                                                #html에 포스트 할 데이터
+        req = {'sequence':[],'shots':[]}                                                               
         for SeqID in selectedIds:
             filters =[                                                                              
                 ['project', 'is', {'type': 'Project', 'id': int(dataDictForm['project_id'])}],
@@ -30,19 +30,19 @@ def delSequence():
             ]
             fields= ['code','shots']
 
-            sg_Seq=sg.find_one('Sequence',filters,fields)                                                #해당 프로젝트내에서 선택된 시퀸스의 code와 shots필드를 포함한 형태의 entity를 로드
+            sg_Seq=sg.find_one('Sequence',filters,fields)                                                
             
-            sg_Shots=sg_Seq['shots']                                                                     #시퀸스에 포함된 샷들로드
+            sg_Shots=sg_Seq['shots']                                                                     
             for sg_Shot in sg_Shots:                      
-                sg_Shot['Episode']=sg_Seq['code']                                                       #html에 종속된 sequence를 포함하기위해 필드 추가
+                sg_Shot['Episode']=sg_Seq['code']                                                      
                 sg_shotExtend=sg.find_one('Shot',[['id', 'is', int(sg_Shot['id'])]],['image'])
-                image=sg_shotExtend['image']                                                            #샷의 썸네일 로드
-                if image == None:                                                                       #없을 시 None
+                image=sg_shotExtend['image']                                                            
+                if image == None:                                                                       
                     sg_Shot['image']="None"
-                else:                                                                                   #있을 시 URL맵핑
+                else:                                                                                  
                     sg_Shot['image']=image
-                sg_Shot.pop('type')                                                                     #html에서 표기 안하기 위해 type필드 제거
-                req['shots'].append(sg_Shot)                                                            #샷 데이터 추가
+                sg_Shot.pop('type')                                                                    
+                req['shots'].append(sg_Shot)                                                           
 
 
             sg_Seq.pop('shots')
@@ -54,7 +54,7 @@ def delSequence():
     except Exception as e:
         return render_template("error.html", data=req)       
     except shotgun_api3.ShotgunError or Exception as e:
-        return render_template("error.html", data=req)                                                  #에러 페이지
+        return render_template("error.html", data=req)                                              
     
 @app.route('/delImplement',methods=['POST'])
 def delImplement():
